5aa0e8fc225ceed73b40453ea53eab818f6c9acd
emilydev <emilse.a.b@gmail.com>
2023-09-26 12:35:54 -0500

    se carga PDF accesible

--- src/config/global.js
+++ src/config/global.js
@@ -123,7 +123,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/21710113-CF01-DU.zip',
       },
       {
         icono: 'fas fa-download',
